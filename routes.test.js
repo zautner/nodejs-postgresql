fbbdb4d1c60dfd9d27a77c2eb2285c3843d940cc
Vitaly F Zautner <zautner@gmail.com>
2021-05-10 17:47:12 +0300

    [*] fixing log entries

--- routes.test.js
+++ routes.test.js
@@ -4,12 +4,14 @@ axios.defaults.adapter = require('axios/lib/adapters/http')
 
 test('index returns alive', async () => {
   const ret = await axios.get(`http://localhost:${config.port}`)
+  console.log('checkng local server')
   expect(ret.data.message).toBe('alive')
 })
 test('quotas', async () => {
   const ret = await axios.get(`http://localhost:${config.port}/quotes?page=1`)
   const array10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   array10.forEach((v, i, a) => {
+  console.log(`expected id: ${i+1}`)
     expect(ret.data?.data?.[i].id).toBe((i + 1).toString())
   })
 })
--- services/db.js
+++ services/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg')
 const config = require('../config')
 const pool = new Pool(config.db)
-
+console.log({pool})
 /**
  * Query the database using the pool
  * @param {*} query
